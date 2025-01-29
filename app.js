0e22c3649b05268af2aa0e53bb890ff4b12d7cac
harenmi <haren.m10@gmail.com>
2025-01-28 22:31:27 -0500

    Implementar una función para agregar amigos.

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 // Declarando variable de tipo array, que almacenará los nombres de los amigos secretos.
-let amigosSecretos = [];
+let amigos = [];
+
+function agregarAmigo() {
+    // Capturar el valor del campo de entrada.
+    let nuevoAmigo = document.getElementById('amigo').value;
+
+    // Validar la entrada.
+    if (nuevoAmigo == '') {
+        alert('Por favor, inserte un nombre.');
+    } else {
+        // Actualizar el array de amigos.
+        amigos.push(nuevoAmigo);
+        // Limpiar el campo de entrada.
+        document.querySelector('#amigo').value = ''; //document.getElementById('amigo').value = '';
+    }
+    return;
+}
