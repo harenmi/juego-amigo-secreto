cc53440e4961da085d8007ccf1277f1d485c0fff
harenmi <haren.m10@gmail.com>
2025-01-28 23:55:18 -0500

    Implementar una función para actualizar la lista de amigos.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Declarando variable de tipo array, que almacenará los nombres de los amigos secretos.
+// Declarando variable de tipo amigos, que almacenará los nombres de los amigos.
 let amigos = [];
 
 function agregarAmigo() {
@@ -9,10 +9,27 @@ function agregarAmigo() {
     if (nuevoAmigo == '') {
         alert('Por favor, inserte un nombre.');
     } else {
-        // Actualizar el array de amigos.
+        // Actualizar el amigos de amigos.
         amigos.push(nuevoAmigo);
+        actualizarListaDeAmigos();
         // Limpiar el campo de entrada.
         document.querySelector('#amigo').value = ''; //document.getElementById('amigo').value = '';
+    }
+    return;
+}
+
+function actualizarListaDeAmigos() {
+    // Obtener el elemento de la lista <ul>.
+    let listaUlHtml = document.getElementById('listaAmigos');
+    // Limpiar la lista existente.
+    listaUlHtml.innerHTML = '';
+    // Iterar sobre el arreglo.
+    for (let i = 0; i < amigos.length; i++) {
+        // Crear elemento de lista <li>, para cada amigo.
+        let elementoIlHtml = document.createElement('li');
+        elementoIlHtml.textContent = amigos[i]
+        // Agregar elementos(amigos) a la lista <ul>.
+        listaUlHtml.appendChild(elementoIlHtml);
     }
     return;
 }
